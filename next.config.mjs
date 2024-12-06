c0ed766d47d17b9de6c138f9e3ec54870952b676
5anne <tahaminaanne@gmail.com>
2024-12-06 20:30:31 +0600

    path_set

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,27 @@
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+
+    images: {
+        remotePatterns: [
+            {
+                protocol: "https",
+                hostname: "*",
+                port: "",
+                pathname: "/**"
+            }
+        ]
+    },
+
+    async redirects() {
+        return [
+            {
+                source: '/',
+                destination: '/',
+                permanent: true,
+            },
+        ]
+    },
+};
 
 export default nextConfig;
