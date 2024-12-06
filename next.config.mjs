bb262fc950a7ed554b81c6a616a090c9bc0db4bb
5anne <tahaminaanne@gmail.com>
2024-12-06 21:18:11 +0600

    config_file_corrected

--- next.config.mjs
+++ next.config.mjs
@@ -12,16 +12,6 @@ const nextConfig = {
             }
         ]
     },
-
-    async redirects() {
-        return [
-            {
-                source: '/',
-                destination: '/',
-                permanent: true,
-            },
-        ]
-    },
 };
 
 export default nextConfig;
